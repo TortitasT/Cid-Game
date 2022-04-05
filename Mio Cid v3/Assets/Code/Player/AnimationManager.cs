c1e86fbb108d45b5cac1c739d9709af511fe698e
Tortitas <victorgf2011@gmail.com>
2022-04-05 18:54:46 +0200

    fix

--- Mio Cid v3/Assets/Code/Player/AnimationManager.cs
+++ Mio Cid v3/Assets/Code/Player/AnimationManager.cs
@@ -5,13 +5,20 @@ using UnityEngine;
 public class AnimationManager : MonoBehaviour
 {
     private Rigidbody2D rb;
+    private Vector2 vel = Vector2.zero;
     [SerializeField] private Animator animator;
 
     private void Start() {
         rb = GetComponent<Rigidbody2D>();
     }
     private void Update() {
-        Vector2 vel = rb.velocity;
+        if(Vector2.Distance((Vector2)rb.velocity, Vector2.zero) >= 1) { // If the player is moving set the animation, if not, leave it as it was to keep facing that way.
+            vel = rb.velocity;
+            animator.SetBool("isWalking", true);
+        }
+        else {
+            animator.SetBool("isWalking", false);
+        }
 
         animator.SetFloat("VelX", vel.x);
         animator.SetFloat("VelY", vel.y);
