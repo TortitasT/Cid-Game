3a8a7d8f8eb753145f2baaad20c848f540c01f3b
Tortitas <victorgf2011@gmail.com>
2022-04-07 12:41:27 +0200

    Removed log

--- Mio Cid v3/Assets/Code/Network/NetworkPlayer.cs
+++ Mio Cid v3/Assets/Code/Network/NetworkPlayer.cs
@@ -8,6 +8,5 @@ public class NetworkPlayer : MonoBehaviour
 
     private void Update() {
         GetComponent<Rigidbody2D>().MovePosition(new Vector3(player.pos.x, player.pos.y, 0));
-        Debug.Log(GetComponent<Rigidbody2D>().velocity);
     }
 }
